6db92aff8d0e66d0690cb767a5cedaa35ccd3617
crbiitr <crbiitr@gmail.com>
2020-07-04 19:45:11 +0530

    project setup

---
+++ common/src/main/java/com/crbiitr/CommonApplication.java
@@ -0,0 +1,13 @@
+package com.crbiitr;
+
+/**
+ * Hello world!
+ *
+ */
+public class CommonApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ioc/src/main/java/com/crbiitr/IocApplication.java
@@ -0,0 +1,13 @@
+package com.crbiitr;
+
+/**
+ * Hello world!
+ *
+ */
+public class IocApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
