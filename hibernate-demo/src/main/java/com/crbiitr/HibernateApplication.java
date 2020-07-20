d509381040fb2c3833adf72502a30b858ba156db
crbiitr <crbiitr@gmail.com>
2020-07-20 21:47:51 +0530

    Added hibernate demo

---
+++ hibernate-demo/src/main/java/com/crbiitr/HibernateApplication.java
@@ -0,0 +1,13 @@
+package com.crbiitr;
+
+/**
+ * Hello world!
+ *
+ */
+public class HibernateApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
